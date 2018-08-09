53b4f595d686a0657e246ff6778bf161d434a0cc
高小川 <gaoxiaochuan@hotmail.com>
2018-08-09 18:02:50 +0800

    Update application.py

--- cas/models/application.py
+++ cas/models/application.py
@@ -8,4 +8,10 @@ class Application(db.Model):
     available = db.Column(db.Boolean, nullable=False)
     tgt_expire = db.Column(db.Integer, nullable=False)
     st_expire = db.Column(db.Integer, nullable=False)
-
+    
+    def existDomain(self, domain):
+        result = self.query.filter(Application.domain == domain).first()
+        return result is not None
+    
+    def get(self, domain):
+        return self.query.filter(Application.domain == domain).first()
