e61a37e6c905f05b76fffd171ffd2c011c77ef47
高小川 <gaoxiaochuan@hotmail.com>
2018-08-10 11:05:52 +0800

    Update cas_schema.sql

--- cas_schema.sql
+++ cas_schema.sql
@@ -13,3 +13,18 @@ create table application_user (
     password char(64) not null,
     available tinyint not null
 );
+
+create table ticket_grant_ticket (
+    token varchar(64) primary key,
+    username varchar(64) not null,
+    create_time TIMESTAMP not null,
+    expire TIMESTAMP not null
+);
+
+create table server_ticket (
+    token varchar(64) primary key,
+    username varchar(64) not null,
+    domain varchar(64) not null,
+    create_time TIMESTAMP not null,
+    expire TIMESTAMP not null
+);
