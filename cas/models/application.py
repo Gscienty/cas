a2132f3f4cbf87b9c34f2d82dd6efc5ecd0c00dc
高小川 <gaoxiaochuan@hotmail.com>
2018-08-10 14:44:28 +0800

    Update application.py

--- cas/models/application.py
+++ cas/models/application.py
@@ -5,6 +5,7 @@ class Application(db.Model):
 
     domain = db.Column(db.String(64), primary_key=True)
     name = db.Column(db.String(32), nullable=False)
+    callback_url = db.Column(db.String(128), nullable=False)
     available = db.Column(db.Boolean, nullable=False)
     tgt_expire = db.Column(db.Integer, nullable=False)
     st_expire = db.Column(db.Integer, nullable=False)
