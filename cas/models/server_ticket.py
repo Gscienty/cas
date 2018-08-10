9cfc66dc490c71f2b8b72c94d7114f5289c83995
高小川 <gaoxiaochuan@hotmail.com>
2018-08-10 14:28:18 +0800

    Update server_ticket.py

--- cas/models/server_ticket.py
+++ cas/models/server_ticket.py
@@ -1,4 +1,8 @@
 from cas.extensions import db
+from flask_sqlalchemy import sqlalchemy
+import uuid
+import time
+import datetime
 
 class ServerTicket(db.Model):
     __tablename__ = 'server_ticket'
@@ -8,3 +12,35 @@ class ServerTicket(db.Model):
     domain = db.Column(db.String(64), nullable=False)
     create_time = db.Column(db.TIMESTAMP, nullable=False)
     expire = db.Column(db.TIMESTAMP, nullable=False)
+    
+    def create(domain, username, expireDuration):
+        originToken = username + str(time.time())
+        token = uuid.uuid5(uuid.NAMESPACE_OID, originToken)
+        expire = datetime.datetime.now() + datetime.timedelta(seconds=expireDuration)
+        
+        st = ServerTicket()
+        
+        st.token = token
+        st.username = username
+        st.domain = domain
+        st.create_time = datetime.datetime.now()
+        st.expire = expire
+        
+        db.session.add(st)
+        db.session.commit(st)
+        
+        return token
+    
+    def __get(token):
+        return ServerTicket.query.filter(sqlalchemy.and_(ServerTicket.token == token,
+                                                         ServerTicket.expire >= datetime.datetime.now())).first()
+    
+    def exist(token):
+        st = ServerTicket.__get(token)    
+        return st is not None
+    
+    def get(token):
+        st = ServerTicket.__get(token)
+        if st is None:
+            return None
+        return st.username
