72db7f561d2155b2c6c9ba157acbf8feb0a0450f
高小川 <gaoxiaochuan@hotmail.com>
2018-08-10 15:24:06 +0800

    Update user_permission.py

--- cas/models/user_permission.py
+++ cas/models/user_permission.py
@@ -1,4 +1,5 @@
 from cas.extensions import db
+from flask_sqlalchemy import sqlalchemy
 
 class UserPermission(db.Model):
     __tablename__ = 'user_permission'
@@ -7,3 +8,13 @@ class UserPermission(db.Model):
     domain = db.Column(db.String(64), nullable=False)
     tag = db.Column(db.String(32), nullable=False)
     available = db.Column(db.Boolean, nullable=False)
+
+    def tags(username, domain):
+        permissions = UserPermission.query.filter(sqlalchemy.and_(UserPermission.username == username,
+                                                                  UserPermission.domain == domain)).all()
+        results = set()
+        for permission in permissions:
+            if permission.available is False:
+                continue
+            results.add(permission.tag)
+        return results
