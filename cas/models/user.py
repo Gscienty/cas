1937a38bb5782c72737481369f3d38721194b24d
高小川 <gaoxiaochuan@hotmail.com>
2018-08-10 11:58:29 +0800

    add profile

--- cas/models/user.py
+++ cas/models/user.py
@@ -7,6 +7,7 @@ class User(db.Model):
     username = db.Column(db.String(64), primary_key=True)
     password = db.Column(db.String(64), nullable=False)
     available = db.Column(db.Boolean, nullable=False)
+    profile = db.Column(db.Text, nullable=True)
 
     def exist(username, password):
         result = User.query.filter(sqlalchemy.and_(User.username == username, User.password == password)).first()
